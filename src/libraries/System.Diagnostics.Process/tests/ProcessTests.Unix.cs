dotnet/corefx

commit 4214b09864bea0416f17727ffa39a55d950035d1

Linux: determine ProcessName using /proc cmdline to avoid truncated names (#37144)
* Linux: determine ProcessName using /proc cmdline to avoid truncated names

* PR feedback

* LongProcessNamesAreSupported test: ensure Process gets killed when Assert throws

src/Common/src/Interop/Linux/procfs/Interop.ProcFsStat.cs
@@ -15,12 +15,14 @@ internal static partial class Interop
     {
         internal const string RootPath = "/proc/";
         private const string ExeFileName = "/exe";
+        private const string CmdLineFileName = "/cmdline";
         private const string StatFileName = "/stat";
         private const string MapsFileName = "/maps";
         private const string FileDescriptorDirectoryName = "/fd/";
         private const string TaskDirectoryName = "/task/";
 
         internal const string SelfExeFilePath = RootPath + "self" + ExeFileName;
+        internal const string SelfCmdLineFilePath = RootPath + "self" + CmdLineFileName;
         internal const string ProcStatFilePath = RootPath + "stat";
 
         internal struct ParsedStat
@@ -31,7 +33,7 @@ internal static partial class Interop
             // the MoveNext() with the appropriate ParseNext* call and assignment.
 
             internal int pid;
-            internal string comm;
+            // internal string comm;
             internal char state;
             internal int ppid;
             //internal int pgrp;
@@ -85,6 +87,11 @@ internal static partial class Interop
         internal static string GetExeFilePathForProcess(int pid)
         {
             return RootPath + pid.ToString(CultureInfo.InvariantCulture) + ExeFileName;
+        }
+
+        internal static string GetCmdLinePathForProcess(int pid)
+        {
+            return RootPath + pid.ToString(CultureInfo.InvariantCulture) + CmdLineFileName;
         }
 
         internal static string GetStatFilePathForProcess(int pid)
@@ -231,7 +238,7 @@ internal static partial class Interop
             var results = default(ParsedStat);
 
             results.pid = parser.ParseNextInt32();
-            results.comm = parser.MoveAndExtractNextInOuterParens();
+            parser.MoveAndExtractNextInOuterParens(extractValue: false); // comm
             results.state = parser.ParseNextChar();
             results.ppid = parser.ParseNextInt32();
             parser.MoveNextOrFail(); // pgrp

src/Common/src/System/IO/StringParser.cs
@@ -98,7 +98,7 @@ namespace System.IO
         /// in the string.  The extracted value will be everything between (not including) those parentheses.
         /// </summary>
         /// <returns></returns>
-        public string MoveAndExtractNextInOuterParens()
+        public string MoveAndExtractNextInOuterParens(bool extractValue = true)
         {
             // Move to the next position
             MoveNextOrFail();
@@ -118,7 +118,7 @@ namespace System.IO
             }
 
             // Extract the contents of the parens, then move our ending position to be after the paren
-            string result = _buffer.Substring(_startIndex + 1, lastParen - _startIndex - 1);
+            string result = extractValue ? _buffer.Substring(_startIndex + 1, lastParen - _startIndex - 1) : null;
             _endIndex = lastParen + 1;
 
             return result;

src/Common/tests/Tests/Interop/procfsTests.cs
@@ -49,7 +49,6 @@ namespace Common.Tests
                 Assert.True(Interop.procfs.TryParseStatFile(path, out result, new ReusableTextReader()));
 
                 Assert.Equal(expectedPid, result.pid);
-                Assert.Equal(expectedComm, result.comm);
                 Assert.Equal(expectedState, result.state);
                 Assert.Equal(expectedSession, result.session);
                 Assert.Equal(expectedUtime, result.utime);

src/System.Diagnostics.Process/src/System/Diagnostics/Process.Linux.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
@@ -29,11 +30,9 @@ namespace System.Diagnostics
             var processes = new List<Process>();
             foreach (int pid in ProcessManager.EnumerateProcessIds())
             {
-                Interop.procfs.ParsedStat parsedStat;
-                if (Interop.procfs.TryReadStatFile(pid, out parsedStat, reusableReader) &&
-                    string.Equals(processName, parsedStat.comm, StringComparison.OrdinalIgnoreCase))
+                if (string.Equals(processName, Process.GetProcessName(pid), StringComparison.OrdinalIgnoreCase))
                 {
-                    ProcessInfo processInfo = ProcessManager.CreateProcessInfo(parsedStat, reusableReader);
+                    ProcessInfo processInfo = ProcessManager.CreateProcessInfo(pid, reusableReader, processName);
                     processes.Add(new Process(machineName, false, processInfo.ProcessId, processInfo));
                 }
             }
@@ -254,6 +253,74 @@ namespace System.Diagnostics
                 Interop.procfs.GetExeFilePathForProcess(processId);
 
             return Interop.Sys.ReadLink(exeFilePath);
+        }
+
+        /// <summary>Gets the name that was used to start the process, or null if it could not be retrieved.</summary>
+        /// <param name="processId">The pid for the target process, or -1 for the current process.</param>
+        internal static string GetProcessName(int processId = -1)
+        {
+            string cmdLineFilePath = processId == -1 ?
+                Interop.procfs.SelfCmdLineFilePath :
+                Interop.procfs.GetCmdLinePathForProcess(processId);
+
+            byte[] rentedArray = null;
+            try
+            {
+                // bufferSize == 1 used to avoid unnecessary buffer in FileStream
+                using (var fs = new FileStream(cmdLineFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 1, useAsync: false))
+                {
+                    Span<byte> buffer = stackalloc byte[512];
+                    int bytesRead = 0;
+                    while (true)
+                    {
+                        // Resize buffer if it was too small.
+                        if (bytesRead == buffer.Length)
+                        {
+                            uint newLength = (uint)buffer.Length * 2;
+
+                            byte[] tmp = ArrayPool<byte>.Shared.Rent((int)newLength);
+                            buffer.CopyTo(tmp);
+                            byte[] toReturn = rentedArray;
+                            buffer = rentedArray = tmp;
+                            if (rentedArray != null)
+                            {
+                                ArrayPool<byte>.Shared.Return(toReturn);
+                            }
+                        }
+
+                        Debug.Assert(bytesRead < buffer.Length);
+                        int n = fs.Read(buffer.Slice(bytesRead));
+                        bytesRead += n;
+
+                        // cmdline contains the argv array separated by '\0' bytes.
+                        // we determine the process name using argv[0].
+                        int argv0End = buffer.Slice(0, bytesRead).IndexOf((byte)'\0');
+                        if (argv0End != -1)
+                        {
+                            // Strip directory names from argv[0].
+                            int nameStart = buffer.Slice(0, argv0End).LastIndexOf((byte)'/') + 1;
+
+                            return Encoding.UTF8.GetString(buffer.Slice(nameStart, argv0End - nameStart));
+                        }
+
+                        if (n == 0)
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                return null;
+            }
+            finally
+            {
+                if (rentedArray != null)
+                {
+                    ArrayPool<byte>.Shared.Return(rentedArray);
+                }
+            }
         }
 
         // ----------------------------------

src/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Linux.cs
@@ -108,7 +108,7 @@ namespace System.Diagnostics
         /// <summary>
         /// Creates a ProcessInfo from the specified process ID.
         /// </summary>
-        internal static ProcessInfo CreateProcessInfo(int pid, ReusableTextReader reusableReader = null)
+        internal static ProcessInfo CreateProcessInfo(int pid, ReusableTextReader reusableReader = null, string processName = null)
         {
             if (reusableReader == null)
             {
@@ -117,21 +117,21 @@ namespace System.Diagnostics
 
             Interop.procfs.ParsedStat stat;
             return Interop.procfs.TryReadStatFile(pid, out stat, reusableReader) ?
-                CreateProcessInfo(stat, reusableReader) :
+                CreateProcessInfo(stat, reusableReader, processName) :
                 null;
         }
 
         /// <summary>
         /// Creates a ProcessInfo from the data parsed from a /proc/pid/stat file and the associated tasks directory.
         /// </summary>
-        internal static ProcessInfo CreateProcessInfo(Interop.procfs.ParsedStat procFsStat, ReusableTextReader reusableReader)
+        internal static ProcessInfo CreateProcessInfo(Interop.procfs.ParsedStat procFsStat, ReusableTextReader reusableReader, string processName)
         {
             int pid = procFsStat.pid;
 
             var pi = new ProcessInfo()
             {
                 ProcessId = pid,
-                ProcessName = procFsStat.comm,
+                ProcessName = processName ?? Process.GetProcessName(pid) ?? string.Empty,
                 BasePriority = (int)procFsStat.nice,
                 VirtualBytes = (long)procFsStat.vsize,
                 WorkingSet = procFsStat.rss * Environment.SystemPageSize,

src/System.Diagnostics.Process/tests/ProcessTests.cs
@@ -1875,6 +1875,37 @@ namespace System.Diagnostics.Tests
             Assert.True(p.HasExited);
         }
 
+        [PlatformSpecific(TestPlatforms.AnyUnix)]
+        [ActiveIssue(37054, TestPlatforms.OSX)]
+        [Fact]
+        public void LongProcessNamesAreSupported()
+        {
+            string programPath = GetProgramPath("sleep");
+
+            if (programPath == null)
+            {
+                return;
+            }
+
+            const string LongProcessName = "123456789012345678901234567890";
+            string sleepCommandPathFileName = Path.Combine(TestDirectory, LongProcessName);
+            File.Copy(programPath, sleepCommandPathFileName);
+
+            using (Process px = Process.Start(sleepCommandPathFileName, "600"))
+            {
+                Process[] runningProcesses = Process.GetProcesses();
+                try
+                {
+                    Assert.Contains(runningProcesses, p => p.ProcessName == LongProcessName);
+                }
+                finally
+                {
+                    px.Kill();
+                    px.WaitForExit();
+                }
+            }
+        }
+
         private string GetCurrentProcessName()
         {
             return $"{Process.GetCurrentProcess().ProcessName}.exe";

src/System.Diagnostics.Process/tests/ProcessTestBase.cs
@@ -101,6 +101,16 @@ namespace System.Diagnostics.Tests
         /// <returns></returns>
         protected static bool IsProgramInstalled(string program)
         {
+            return GetProgramPath(program) != null;
+        }
+
+        /// <summary>
+        /// Return program path
+        /// </summary>
+        /// <param name="program"></param>
+        /// <returns></returns>
+        protected static string GetProgramPath(string program)
+        {
             string path;
             string pathEnvVar = Environment.GetEnvironmentVariable("PATH");
             char separator = PlatformDetection.IsWindows ? ';' : ':';
@@ -113,11 +123,11 @@ namespace System.Diagnostics.Tests
                     path = Path.Combine(subPath, program);
                     if (File.Exists(path))
                     {
-                        return true;
+                        return path;
                     }
                 }
             }
-            return false;
+            return null;
         }
     }
 }

backend-devloper/runtime

commit 100168654c84f5e646bec8823aa255964f889329

Linux: ProcessName: return script name instead of interpreter program (dotnet/corefx#37294)
* Linux: ProcessName: return script name instead of interpreter program

Fixes https://github.com/dotnet/corefx/issues/37198
Regression by https://github.com/dotnet/corefx/pull/37144

* Add ProcessNameMatchesScriptName test

* ProcessNameMatchesScriptName: don't run on OSX

* LongProcessNamesAreSupported: don't run on Alpine

* Remove ActiveIssue attributes


Commit migrated from https://github.com/dotnet/corefx/commit/1b6f45ca261467baea62ef577f8b8a7c6cf3b96c

src/libraries/Common/src/Interop/Linux/procfs/Interop.ProcFsStat.cs
@@ -33,7 +33,7 @@ internal static partial class Interop
             // the MoveNext() with the appropriate ParseNext* call and assignment.
 
             internal int pid;
-            // internal string comm;
+            internal string comm;
             internal char state;
             internal int ppid;
             //internal int pgrp;
@@ -238,7 +238,7 @@ internal static partial class Interop
             var results = default(ParsedStat);
 
             results.pid = parser.ParseNextInt32();
-            parser.MoveAndExtractNextInOuterParens(extractValue: false); // comm
+            results.comm = parser.MoveAndExtractNextInOuterParens();
             results.state = parser.ParseNextChar();
             results.ppid = parser.ParseNextInt32();
             parser.MoveNextOrFail(); // pgrp

src/libraries/Common/src/System/IO/StringParser.cs
@@ -98,7 +98,7 @@ namespace System.IO
         /// in the string.  The extracted value will be everything between (not including) those parentheses.
         /// </summary>
         /// <returns></returns>
-        public string MoveAndExtractNextInOuterParens(bool extractValue = true)
+        public string MoveAndExtractNextInOuterParens()
         {
             // Move to the next position
             MoveNextOrFail();
@@ -118,7 +118,7 @@ namespace System.IO
             }
 
             // Extract the contents of the parens, then move our ending position to be after the paren
-            string result = extractValue ? _buffer.Substring(_startIndex + 1, lastParen - _startIndex - 1) : null;
+            string result = _buffer.Substring(_startIndex + 1, lastParen - _startIndex - 1);
             _endIndex = lastParen + 1;
 
             return result;

src/libraries/System.Diagnostics.Process/src/System/Diagnostics/Process.Linux.cs
@@ -30,9 +30,11 @@ namespace System.Diagnostics
             var processes = new List<Process>();
             foreach (int pid in ProcessManager.EnumerateProcessIds())
             {
-                if (string.Equals(processName, Process.GetProcessName(pid), StringComparison.OrdinalIgnoreCase))
+                Interop.procfs.ParsedStat parsedStat;
+                if (Interop.procfs.TryReadStatFile(pid, out parsedStat, reusableReader) &&
+                    string.Equals(processName, Process.GetUntruncatedProcessName(ref parsedStat), StringComparison.OrdinalIgnoreCase))
                 {
-                    ProcessInfo processInfo = ProcessManager.CreateProcessInfo(pid, reusableReader, processName);
+                    ProcessInfo processInfo = ProcessManager.CreateProcessInfo(ref parsedStat, reusableReader, processName);
                     processes.Add(new Process(machineName, false, processInfo.ProcessId, processInfo));
                 }
             }
@@ -256,12 +258,10 @@ namespace System.Diagnostics
         }
 
         /// <summary>Gets the name that was used to start the process, or null if it could not be retrieved.</summary>
-        /// <param name="processId">The pid for the target process, or -1 for the current process.</param>
-        internal static string GetProcessName(int processId = -1)
+        /// <param name="stat">The stat for the target process.</param>
+        internal static string GetUntruncatedProcessName(ref Interop.procfs.ParsedStat stat)
         {
-            string cmdLineFilePath = processId == -1 ?
-                Interop.procfs.SelfCmdLineFilePath :
-                Interop.procfs.GetCmdLinePathForProcess(processId);
+            string cmdLineFilePath = Interop.procfs.GetCmdLinePathForProcess(stat.pid);
 
             byte[] rentedArray = null;
             try
@@ -282,7 +282,7 @@ namespace System.Diagnostics
                             buffer.CopyTo(tmp);
                             byte[] toReturn = rentedArray;
                             buffer = rentedArray = tmp;
-                            if (rentedArray != null)
+                            if (toReturn != null)
                             {
                                 ArrayPool<byte>.Shared.Return(toReturn);
                             }
@@ -293,32 +293,62 @@ namespace System.Diagnostics
                         bytesRead += n;
 
                         // cmdline contains the argv array separated by '\0' bytes.
-                        // we determine the process name using argv[0].
-                        int argv0End = buffer.Slice(0, bytesRead).IndexOf((byte)'\0');
-                        if (argv0End != -1)
+                        // stat.comm contains a possibly truncated version of the process name.
+                        // When the program is a native executable, the process name will be in argv[0].
+                        // When the program is a script, argv[0] contains the interpreter, and argv[1] contains the script name.
+                        Span<byte> argRemainder = buffer.Slice(0, bytesRead);
+                        int argEnd = argRemainder.IndexOf((byte)'\0');
+                        if (argEnd != -1)
                         {
-                            // Strip directory names from argv[0].
-                            int nameStart = buffer.Slice(0, argv0End).LastIndexOf((byte)'/') + 1;
+                            // Check if argv[0] has the process name.
+                            string name = GetUntruncatedNameFromArg(argRemainder.Slice(0, argEnd), prefix: stat.comm);
+                            if (name != null)
+                            {
+                                return name;
+                            }
 
-                            return Encoding.UTF8.GetString(buffer.Slice(nameStart, argv0End - nameStart));
+                            // Check if argv[1] has the process name.
+                            argRemainder = argRemainder.Slice(argEnd + 1);
+                            argEnd = argRemainder.IndexOf((byte)'\0');
+                            if (argEnd != -1)
+                            {
+                                name = GetUntruncatedNameFromArg(argRemainder.Slice(0, argEnd), prefix: stat.comm);
+                                return name ?? stat.comm;
+                            }
                         }
 
                         if (n == 0)
                         {
-                            return null;
+                            return stat.comm;
                         }
                     }
                 }
             }
             catch (IOException)
             {
-                return null;
+                return stat.comm;
             }
             finally
             {
                 if (rentedArray != null)
                 {
                     ArrayPool<byte>.Shared.Return(rentedArray);
+                }
+            }
+
+            string GetUntruncatedNameFromArg(Span<byte> arg, string prefix)
+            {
+                // Strip directory names from arg.
+                int nameStart = arg.LastIndexOf((byte)'/') + 1;
+                string argString = Encoding.UTF8.GetString(arg.Slice(nameStart));
+
+                if (argString.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                {
+                    return argString;
+                }
+                else
+                {
+                    return null;
                 }
             }
         }

src/libraries/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Linux.cs
@@ -108,7 +108,7 @@ namespace System.Diagnostics
         /// <summary>
         /// Creates a ProcessInfo from the specified process ID.
         /// </summary>
-        internal static ProcessInfo CreateProcessInfo(int pid, ReusableTextReader reusableReader = null, string processName = null)
+        internal static ProcessInfo CreateProcessInfo(int pid, ReusableTextReader reusableReader = null)
         {
             if (reusableReader == null)
             {
@@ -117,21 +117,21 @@ namespace System.Diagnostics
 
             Interop.procfs.ParsedStat stat;
             return Interop.procfs.TryReadStatFile(pid, out stat, reusableReader) ?
-                CreateProcessInfo(stat, reusableReader, processName) :
+                CreateProcessInfo(ref stat, reusableReader) :
                 null;
         }
 
         /// <summary>
         /// Creates a ProcessInfo from the data parsed from a /proc/pid/stat file and the associated tasks directory.
         /// </summary>
-        internal static ProcessInfo CreateProcessInfo(Interop.procfs.ParsedStat procFsStat, ReusableTextReader reusableReader, string processName)
+        internal static ProcessInfo CreateProcessInfo(ref Interop.procfs.ParsedStat procFsStat, ReusableTextReader reusableReader, string processName = null)
         {
             int pid = procFsStat.pid;
 
             var pi = new ProcessInfo()
             {
                 ProcessId = pid,
-                ProcessName = processName ?? Process.GetProcessName(pid) ?? string.Empty,
+                ProcessName = processName ?? Process.GetUntruncatedProcessName(ref procFsStat) ?? string.Empty,
                 BasePriority = (int)procFsStat.nice,
                 VirtualBytes = (long)procFsStat.vsize,
                 WorkingSet = procFsStat.rss * Environment.SystemPageSize,

src/libraries/Common/tests/Tests/Interop/procfsTests.cs
@@ -49,6 +49,7 @@ namespace Common.Tests
                 Assert.True(Interop.procfs.TryParseStatFile(path, out result, new ReusableTextReader()));
 
                 Assert.Equal(expectedPid, result.pid);
+                Assert.Equal(expectedComm, result.comm);
                 Assert.Equal(expectedState, result.state);
                 Assert.Equal(expectedSession, result.session);
                 Assert.Equal(expectedUtime, result.utime);

src/libraries/System.Diagnostics.Process/tests/ProcessTests.Unix.cs
@@ -124,7 +124,6 @@ namespace System.Diagnostics.Tests
             }
         }
 
-        [ActiveIssue(37198)]
         [Theory, InlineData(true), InlineData(false)]
         [OuterLoop("Opens program")]
         public void ProcessStart_UseShellExecute_OnUnix_SuccessWhenProgramInstalled(bool isFolder)
@@ -153,6 +152,31 @@ namespace System.Diagnostics.Tests
                     px.Kill();
                     px.WaitForExit();
                     Assert.True(px.HasExited);
+                }
+            }
+        }
+
+        [Fact]
+        [PlatformSpecific(~TestPlatforms.OSX)] // On OSX, ProcessName returns the script interpreter.
+        public void ProcessNameMatchesScriptName()
+        {
+            string scriptName = GetTestFileName();
+            string filename = Path.Combine(TestDirectory, scriptName);
+            File.WriteAllText(filename, $"#!/bin/sh\nsleep 600\n"); // sleep 10 min.
+            // set x-bit
+            int mode = Convert.ToInt32("744", 8);
+            Assert.Equal(0, chmod(filename, mode));
+
+            using (var process = Process.Start(new ProcessStartInfo { FileName = filename }))
+            {
+                try
+                {
+                    Assert.Equal(scriptName, process.ProcessName);
+                }
+                finally
+                {
+                    process.Kill();
+                    process.WaitForExit();
                 }
             }
         }
@@ -294,7 +318,6 @@ namespace System.Diagnostics.Tests
             }
         }
 
-        [ActiveIssue(37198)]
         [Theory, InlineData("vi")]
         [PlatformSpecific(TestPlatforms.Linux)]
         [OuterLoop("Opens program")]

src/libraries/System.Diagnostics.Process/tests/ProcessTests.cs
@@ -1921,15 +1921,16 @@ namespace System.Diagnostics.Tests
             Assert.True(p.HasExited);
         }
 
-        [ActiveIssue(37198)]
         [PlatformSpecific(TestPlatforms.AnyUnix)]
         [ActiveIssue(37054, TestPlatforms.OSX)]
         [Fact]
         public void LongProcessNamesAreSupported()
         {
+            // Alpine implements sleep as a symlink to the busybox executable.
+            // If we rename it, the program will no longer sleep.
             if (PlatformDetection.IsAlpine)
             {
-                return; // https://github.com/dotnet/corefx/issues/37054
+                return;
             }
 
             string programPath = GetProgramPath("sleep");
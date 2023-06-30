backend-devloper/runtime

commit f6230892a0a0678c8e78163d13e4ea231584044c

[browser] Unify boot config schema and output layout (#86255)
- Update `WasmAppBuilder` to produce the same boot config schema as `GenerateWasmBootJson`
- Update AppBundle layout to match blazor layout. 
    - All the runtime files (js, wasm, icu) are deployed to `_framework` folder.
    - This behavior can be overriden by setting `<WasmRuntimeAssetsLocation>` property. The empty value is overriden to default `_framework`, but `./` can be used to flatten the output structure.
    - WBT test with flat output
    - The only remaining difference is `AppBundle` vs `wwwrooot` folder name.
- `JSHost.ImportAsync` now requires the same relative paths as in blazor (`./module.js` => `../module.js`)

Contributes to https://github.com/dotnet/runtime/issues/70762
---------

Co-authored-by: Ankit Jain <radical@gmail.com>

src/mono/wasm/runtime/loader/blazor/_Integration.ts
@@ -5,7 +5,7 @@ import type { DotnetModuleInternal, MonoConfigInternal } from "../../types/inter
 import type { AssetBehaviours, AssetEntry, LoadingResource, WebAssemblyBootResourceType, WebAssemblyStartOptions } from "../../types";
 import type { BootJsonData } from "../../types/blazor";
 
-import { INTERNAL, loaderHelpers } from "../globals";
+import { ENVIRONMENT_IS_WEB, INTERNAL, loaderHelpers } from "../globals";
 import { BootConfigResult } from "./BootConfig";
 import { WebAssemblyResourceLoader } from "./WebAssemblyResourceLoader";
 import { hasDebuggingEnabled } from "./_Polyfill";
@@ -22,7 +22,10 @@ export async function loadBootConfig(config: MonoConfigInternal, module: DotnetM
 export async function initializeBootConfig(bootConfigResult: BootConfigResult, module: DotnetModuleInternal, startupOptions?: Partial<WebAssemblyStartOptions>) {
     INTERNAL.resourceLoader = resourceLoader = await WebAssemblyResourceLoader.initAsync(bootConfigResult.bootConfig, startupOptions ?? {});
     mapBootConfigToMonoConfig(loaderHelpers.config, bootConfigResult.applicationEnvironment);
-    setupModuleForBlazor(module);
+
+    if (ENVIRONMENT_IS_WEB) {
+        setupModuleForBlazor(module);
+    }
 }
 
 let resourcesLoaded = 0;
@@ -34,8 +37,9 @@ const behaviorByName = (name: string): AssetBehaviours | "other" => {
             : (name.startsWith("dotnet.native") && name.endsWith(".js")) ? "js-module-native"
                 : (name.startsWith("dotnet.runtime") && name.endsWith(".js")) ? "js-module-runtime"
                     : (name.startsWith("dotnet") && name.endsWith(".js")) ? "js-module-dotnet"
-                        : name.startsWith("icudt") ? "icu"
-                            : "other";
+                        : (name.startsWith("dotnet.native") && name.endsWith(".symbols")) ? "symbols"
+                            : name.startsWith("icudt") ? "icu"
+                                : "other";
 };
 
 const monoToBlazorAssetTypeMap: { [key: string]: WebAssemblyBootResourceType | undefined } = {
@@ -71,29 +75,51 @@ export function setupModuleForBlazor(module: DotnetModuleInternal) {
     };
 
     loaderHelpers.downloadResource = downloadResource; // polyfills were already assigned
-    module.disableDotnet6Compatibility = false;
+}
+
+function appendUniqueQuery(attemptUrl: string): string {
+    if (loaderHelpers.assetUniqueQuery) {
+        attemptUrl = attemptUrl + loaderHelpers.assetUniqueQuery;
+    }
+
+    return attemptUrl;
 }
 
 export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, applicationEnvironment: string) {
     const resources = resourceLoader.bootConfig.resources;
 
     const assets: AssetEntry[] = [];
-    const environmentVariables: any = {};
+    const environmentVariables: any = {
+        // From boot config
+        ...(resourceLoader.bootConfig.environmentVariables || {}),
+        // From JavaScript
+        ...(moduleConfig.environmentVariables || {})
+    };
 
     moduleConfig.applicationEnvironment = applicationEnvironment;
 
+    moduleConfig.remoteSources = (resourceLoader.bootConfig.resources as any).remoteSources;
+    moduleConfig.assetsHash = resourceLoader.bootConfig.resources.hash;
     moduleConfig.assets = assets;
     moduleConfig.globalizationMode = "icu";
-    moduleConfig.environmentVariables = environmentVariables;
-    moduleConfig.debugLevel = hasDebuggingEnabled(resourceLoader.bootConfig) ? 1 : 0;
-    moduleConfig.maxParallelDownloads = 1000000; // disable throttling parallel downloads
-    moduleConfig.enableDownloadRetry = false; // disable retry downloads
+    moduleConfig.debugLevel = hasDebuggingEnabled(resourceLoader.bootConfig) ? resourceLoader.bootConfig.debugLevel : 0;
     moduleConfig.mainAssemblyName = resourceLoader.bootConfig.entryAssembly;
+
+    const anyBootConfig = (resourceLoader.bootConfig as any);
+    for (const key in resourceLoader.bootConfig) {
+        if (Object.prototype.hasOwnProperty.call(anyBootConfig, key)) {
+            if (anyBootConfig[key] === null) {
+                delete anyBootConfig[key];
+            }
+        }
+    }
 
     // FIXME this mix of both formats is ugly temporary hack
     Object.assign(moduleConfig, {
         ...resourceLoader.bootConfig,
     });
+
+    moduleConfig.environmentVariables = environmentVariables;
 
     if (resourceLoader.bootConfig.startupMemoryCache !== undefined) {
         moduleConfig.startupMemoryCache = resourceLoader.bootConfig.startupMemoryCache;
@@ -107,13 +133,13 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
     for (const name in resources.runtimeAssets) {
         const asset = resources.runtimeAssets[name] as AssetEntry;
         asset.name = name;
-        asset.resolvedUrl = loaderHelpers.locateFile(name);
+        asset.resolvedUrl = appendUniqueQuery(loaderHelpers.locateFile(name));
         assets.push(asset);
     }
     for (const name in resources.assembly) {
         const asset: AssetEntry = {
             name,
-            resolvedUrl: loaderHelpers.locateFile(name),
+            resolvedUrl: appendUniqueQuery(loaderHelpers.locateFile(name)),
             hash: resources.assembly[name],
             behavior: "assembly",
         };
@@ -123,18 +149,19 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
         for (const name in resources.pdb) {
             const asset: AssetEntry = {
                 name,
-                resolvedUrl: loaderHelpers.locateFile(name),
+                resolvedUrl: appendUniqueQuery(loaderHelpers.locateFile(name)),
                 hash: resources.pdb[name],
                 behavior: "pdb",
             };
             assets.push(asset);
         }
     }
-    const applicationCulture = resourceLoader.startOptions.applicationCulture || (navigator.languages && navigator.languages[0]);
-    const icuDataResourceName = getICUResourceName(resourceLoader.bootConfig, applicationCulture);
+    const applicationCulture = resourceLoader.startOptions.applicationCulture || ENVIRONMENT_IS_WEB ? (navigator.languages && navigator.languages[0]) : Intl.DateTimeFormat().resolvedOptions().locale;
+    const icuDataResourceName = getICUResourceName(resourceLoader.bootConfig, moduleConfig, applicationCulture);
     let hasIcuData = false;
     for (const name in resources.runtime) {
         const behavior = behaviorByName(name) as any;
+        let loadRemote = false;
         if (behavior === "icu") {
             if (resourceLoader.bootConfig.icuDataMode === ICUDataMode.Invariant) {
                 continue;
@@ -142,6 +169,7 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
             if (name !== icuDataResourceName) {
                 continue;
             }
+            loadRemote = true;
             hasIcuData = true;
         } else if (behavior === "js-module-dotnet") {
             continue;
@@ -149,23 +177,51 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
             continue;
         }
 
-        const resolvedUrl = loaderHelpers.locateFile(name);
+        const resolvedUrl = appendUniqueQuery(loaderHelpers.locateFile(name));
         const asset: AssetEntry = {
             name,
             resolvedUrl,
             hash: resources.runtime[name],
             behavior,
+            loadRemote
         };
         assets.push(asset);
     }
+
+    if (moduleConfig.loadAllSatelliteResources && resources.satelliteResources) {
+        for (const culture in resources.satelliteResources) {
+            for (const name in resources.satelliteResources[culture]) {
+                assets.push({
+                    name,
+                    culture,
+                    behavior: "resource",
+                    hash: resources.satelliteResources[culture][name],
+                });
+            }
+        }
+    }
+
     for (let i = 0; i < resourceLoader.bootConfig.config.length; i++) {
         const config = resourceLoader.bootConfig.config[i];
         if (config === "appsettings.json" || config === `appsettings.${applicationEnvironment}.json`) {
             assets.push({
                 name: config,
-                resolvedUrl: (document ? document.baseURI : "/") + config,
+                resolvedUrl: appendUniqueQuery((document ? document.baseURI : "/") + config),
                 behavior: "vfs",
             });
+        }
+    }
+
+    for (const virtualPath in resources.vfs) {
+        for (const name in resources.vfs[virtualPath]) {
+            const asset: AssetEntry = {
+                name,
+                resolvedUrl: appendUniqueQuery(loaderHelpers.locateFile(name)),
+                hash: resources.vfs[virtualPath][name],
+                behavior: "vfs",
+                virtualPath
+            };
+            assets.push(asset);
         }
     }
 
@@ -192,24 +248,26 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
     }
 }
 
-function getICUResourceName(bootConfig: BootJsonData, culture: string | undefined): string {
+function getICUResourceName(bootConfig: BootJsonData, moduleConfig: MonoConfigInternal, culture: string | undefined): string {
     if (bootConfig.icuDataMode === ICUDataMode.Custom) {
         const icuFiles = Object
             .keys(bootConfig.resources.runtime)
             .filter(n => n.startsWith("icudt") && n.endsWith(".dat"));
         if (icuFiles.length === 1) {
+            moduleConfig.globalizationMode = "icu";
             const customIcuFile = icuFiles[0];
             return customIcuFile;
         }
     }
 
-    if (bootConfig.icuDataMode === ICUDataMode.Hybrid)
-    {
+    if (bootConfig.icuDataMode === ICUDataMode.Hybrid) {
+        moduleConfig.globalizationMode = "hybrid";
         const reducedICUResourceName = "icudt_hybrid.dat";
         return reducedICUResourceName;
     }
 
     if (!culture || bootConfig.icuDataMode === ICUDataMode.All) {
+        moduleConfig.globalizationMode = "icu";
         const combinedICUResourceName = "icudt.dat";
         return combinedICUResourceName;
     }

src/libraries/System.Runtime.InteropServices.JavaScript/tests/System.Runtime.InteropServices.JavaScript.Legacy.UnitTests/System/Runtime/InteropServices/JavaScript/TimerTests.cs
@@ -90,7 +90,7 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
         {
             if (_module == null)
             {
-                _module = await JSHost.ImportAsync("Timers", "./timers.mjs");
+                _module = await JSHost.ImportAsync("Timers", "../timers.mjs");
             }
         }
 

src/libraries/System.Runtime.InteropServices.JavaScript/tests/System.Runtime.InteropServices.JavaScript.UnitTests/System/Runtime/InteropServices/JavaScript/JSImportExportTest.cs
@@ -22,8 +22,8 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
         [Fact]
         public async Task MultipleImportAsync()
         {
-            var first = await JSHost.ImportAsync("JavaScriptTestHelper", "./JavaScriptTestHelper.mjs");
-            var second = await JSHost.ImportAsync("JavaScriptTestHelper", "./JavaScriptTestHelper.mjs");
+            var first = await JSHost.ImportAsync("JavaScriptTestHelper", "../JavaScriptTestHelper.mjs");
+            var second = await JSHost.ImportAsync("JavaScriptTestHelper", "../JavaScriptTestHelper.mjs");
             Assert.NotNull(first);
             Assert.NotNull(second);
             Assert.Equal("object", first.GetTypeOfProperty("instance"));
@@ -36,12 +36,12 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
         public async Task CancelableImportAsync()
         {
             var cts = new CancellationTokenSource();
-            var exTask = Assert.ThrowsAsync<JSException>(async () => await JSHost.ImportAsync("JavaScriptTestHelper", "./JavaScriptTestHelper.mjs", cts.Token));
+            var exTask = Assert.ThrowsAsync<JSException>(async () => await JSHost.ImportAsync("JavaScriptTestHelper", "../JavaScriptTestHelper.mjs", cts.Token));
             cts.Cancel();
             var actualEx2 = await exTask;
             Assert.Equal("OperationCanceledException", actualEx2.Message);
 
-            var actualEx = await Assert.ThrowsAsync<JSException>(async () => await JSHost.ImportAsync("JavaScriptTestHelper", "./JavaScriptTestHelper.mjs", new CancellationToken(true)));
+            var actualEx = await Assert.ThrowsAsync<JSException>(async () => await JSHost.ImportAsync("JavaScriptTestHelper", "../JavaScriptTestHelper.mjs", new CancellationToken(true)));
             Assert.Equal("OperationCanceledException", actualEx.Message);
         }
 
@@ -300,7 +300,7 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
             yield return new object[] { new object[] { "JSData" } }; // special cased, so we call createData in the test itself
             yield return new object[] { new object[] { new byte[] { }, new int[] { }, new double[] { }, new string[] { }, new object[] { } } };
             yield return new object[] { new object[] { new byte[] { 1, 2, 3 }, new int[] { 1, 2, 3 }, new double[] { 1, 2, 3 }, new string[] { "a", "b", "c" }, new object[] { } } };
-            yield return new object[] { new object[] { new object[] { new byte[] { 1, 2, 3 }, new int[] { 1, 2, 3 }, new double[] { 1, 2, 3 }, new string[] { "a", "b", "c" } , new object(), new SomethingRef(), new SomethingStruct(), new Exception("test") } } };
+            yield return new object[] { new object[] { new object[] { new byte[] { 1, 2, 3 }, new int[] { 1, 2, 3 }, new double[] { 1, 2, 3 }, new string[] { "a", "b", "c" }, new object(), new SomethingRef(), new SomethingStruct(), new Exception("test") } } };
             yield return new object[] { new object[] { } };
             yield return new object[] { null };
         }
@@ -317,10 +317,10 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
             Assert.Equal(expected, actual);
 
             if (expected != null) for (int i = 0; i < expected.Length; i++)
-            {
-                var actualI = JavaScriptTestHelper.store_ObjectArray(expected, i);
-                Assert.Equal(expected[i], actualI);
-            }
+                {
+                    var actualI = JavaScriptTestHelper.store_ObjectArray(expected, i);
+                    Assert.Equal(expected[i], actualI);
+                }
         }
 
         public static IEnumerable<object[]> MarshalObjectArrayCasesToDouble()
@@ -362,7 +362,7 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
             yield return new object[] { new object[] { (ushort)0 } };
             yield return new object[] { new object[] { new SomethingStruct[] { } } };
             yield return new object[] { new object[] { new SomethingRef[] { }, } };
-            yield return new object[] { new object[] { new ArraySegment<byte>(new byte[] { 11 }) , } };
+            yield return new object[] { new object[] { new ArraySegment<byte>(new byte[] { 11 }), } };
         }
         delegate void dummyDelegate();
         static void dummyDelegateA()

src/libraries/System.Runtime.InteropServices.JavaScript/tests/System.Runtime.InteropServices.JavaScript.UnitTests/System/Runtime/InteropServices/JavaScript/JavaScriptTestHelper.cs
@@ -68,7 +68,7 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
         [JSExport]
         public static void Optimized1V(int a1)
         {
-            optimizedReached+= a1;
+            optimizedReached += a1;
         }
         [JSImport("invoke1V", "JavaScriptTestHelper")]
         public static partial void invoke1V(int a1);
@@ -85,8 +85,8 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
         [JSExport]
         public static int Optimized2R(int a1, int a2)
         {
-            optimizedReached += a1+ a2;
-            return a1 + a2 +1;
+            optimizedReached += a1 + a2;
+            return a1 + a2 + 1;
         }
         [JSImport("invoke2R", "JavaScriptTestHelper")]
         public static partial int invoke2R(int a1, int a2);
@@ -997,7 +997,7 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
             if (_module == null)
             {
                 // Log("JavaScriptTestHelper.mjs importing");
-                _module = await JSHost.ImportAsync("JavaScriptTestHelper", "./JavaScriptTestHelper.mjs");
+                _module = await JSHost.ImportAsync("JavaScriptTestHelper", "../JavaScriptTestHelper.mjs");
                 await Setup();
                 // Log("JavaScriptTestHelper.mjs imported");
             }
@@ -1010,7 +1010,7 @@ namespace JavaScriptTestHelperNamespace
     public partial class JavaScriptTestHelper
     {
         [System.Runtime.InteropServices.JavaScript.JSExport]
-        public static string EchoString(string message) 
+        public static string EchoString(string message)
         {
             return message + "11";
         }
@@ -1019,7 +1019,7 @@ namespace JavaScriptTestHelperNamespace
         {
             [System.Runtime.InteropServices.JavaScript.JSExport]
             public static string EchoString(string message) => message + "12";
-        
+
             private partial class DoubleNestedClass
             {
                 [System.Runtime.InteropServices.JavaScript.JSExport]

src/libraries/System.Runtime.InteropServices.JavaScript/tests/System.Runtime.InteropServices.JavaScript.UnitTests/System/Runtime/InteropServices/JavaScript/SecondRuntimeTest.cs
@@ -12,9 +12,9 @@ namespace System.Runtime.InteropServices.JavaScript.Tests
     public partial class SecondRuntimeTest
     {
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsBrowserDomSupportedOrNodeJS))]
-        public static async Task RunSecondRuntimeAndTestStaticState() 
+        public static async Task RunSecondRuntimeAndTestStaticState()
         {
-            await JSHost.ImportAsync("SecondRuntimeTest", "./SecondRuntimeTest.js");
+            await JSHost.ImportAsync("SecondRuntimeTest", "../SecondRuntimeTest.js");
 
             Interop.State = 42;
             var state2 = await Interop.RunSecondRuntimeAndTestStaticState();

src/libraries/System.Runtime.InteropServices.JavaScript/tests/System.Runtime.InteropServices.JavaScript.UnitTests/System/Runtime/InteropServices/JavaScript/SecondRuntimeTest.js
@@ -1,5 +1,5 @@
 export async function runSecondRuntimeAndTestStaticState() {
-    const { dotnet: dotnet2 } = await import('./dotnet.js?instance=2');
+    const { dotnet: dotnet2 } = await import('./_framework/dotnet.js?instance=2');
     const runtime2 = await dotnet2
         .withStartupMemoryCache(false)
         .withConfig({

src/mono/sample/mbr/browser/main.js
@@ -1,4 +1,4 @@
-import { dotnet } from './dotnet.js'
+import { dotnet } from './_framework/dotnet.js'
 
 try {
     const { getAssemblyExports } = await dotnet
@@ -8,7 +8,7 @@ try {
     const exports = await getAssemblyExports("WasmDelta.dll");
     const update = exports.Sample.Test.Update;
     const testMeaning = exports.Sample.Test.TestMeaning;
-    
+
     const outElement = document.getElementById("out");
     document.getElementById("update").addEventListener("click", function () {
         update();

src/mono/sample/wasm/browser-advanced/Wasm.Advanced.Sample.csproj
@@ -14,6 +14,9 @@
     <_ServeHeaders>$(_ServeHeaders) -h &quot;Content-Security-Policy: default-src 'self' 'wasm-unsafe-eval'&quot;</_ServeHeaders>
     <!-- enable reporting to profiler in browser dev tools -->
     <WasmProfilers>browser;</WasmProfilers>
+
+    <!-- Put "framework" (dotnet.js, dlls, etc) files directly into the AppBundle -->
+    <WasmRuntimeAssetsLocation>./</WasmRuntimeAssetsLocation>
   </PropertyGroup>
   <ItemGroup>
     <WasmExtraFilesToDeploy Include="main.js" />

commit 0f1333d621145f07c0c56b3d708bc01dab4ef621

[browser] Fix missing braces when resolving application culture (#88264)
Regression from https://github.com/dotnet/runtime/pull/86255.

src/mono/wasm/runtime/loader/blazor/_Integration.ts
@@ -156,7 +156,7 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
             assets.push(asset);
         }
     }
-    const applicationCulture = resourceLoader.startOptions.applicationCulture || ENVIRONMENT_IS_WEB ? (navigator.languages && navigator.languages[0]) : Intl.DateTimeFormat().resolvedOptions().locale;
+    const applicationCulture = resourceLoader.startOptions.applicationCulture || (ENVIRONMENT_IS_WEB ? (navigator.languages && navigator.languages[0]) : Intl.DateTimeFormat().resolvedOptions().locale);
     const icuDataResourceName = getICUResourceName(resourceLoader.bootConfig, moduleConfig, applicationCulture);
     let hasIcuData = false;
     for (const name in resources.runtime) {